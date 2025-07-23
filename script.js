1503e61127f952962db99baf637eadda5da3e3c4
honeylemonicetea <dark.shadow2019@yahoo.com>
2025-07-24 01:27:46 +0300

    formating

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // about
-
 let aboutButton = document.querySelector(".about-link")
 let aboutContent = document.querySelector(".nav__about-content")
 
@@ -7,11 +6,11 @@ aboutButton.onclick = function(){
     aboutContent.classList.toggle("about_open")
 }
 
-
 // кнопка вверх
 let team = document.querySelector(".team")
 let menuHeight = document.querySelector(".nav").clientHeight
 let upButton = document.querySelector(".up-button")
+
 document.onscroll=function(){
     if (team.getBoundingClientRect().top < menuHeight){
         upButton.style.display = 'flex'
@@ -21,12 +20,11 @@ document.onscroll=function(){
     }
 }
 
-
-
 // модальное окно
 //открыть
 let addButton = document.querySelectorAll(".team__add-button")
 let modal = document.querySelector(".team__modal")
+
 function openModal(event){
     modal.style.display = 'flex'    
     let name = event.target.parentElement.children[0].innerHTML
@@ -49,14 +47,11 @@ modal.onclick =  function(event){
 let year = document.querySelector(".footer__copyright-year")
 year.innerHTML = new Date().getFullYear()
 
-
-
 // бургер меню
 
 let burgerButton = document.querySelector(".nav__burger-button")
 let burgerContainer = document.querySelector(".nav__burger")
 let menuLinks = burgerContainer.querySelectorAll("a")
-
 
 burgerButton.onclick = function(){
     burgerContainer.classList.toggle("nav__burger_open")
